110042a53efe24cae65c9942157dbd379fb01b88
Martin Žilák <martin.zilak@mailbox.org>
2024-11-30 22:46:48 +0100

    Content WIP.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './styles.module.css';
+import classNames from 'classnames';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.a4page}>
+      <div className={styles.headline}>
+        <span>Martin&nbsp;</span>
+        <span className={classNames(styles.orange, styles.bold)}>Ži</span>
+        <span className={styles.bold}>lák</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className={styles.content}>
+        <div className={styles.sidebar}>
+          <div>
+            <span className={styles.sidebarHeadline}>contact</span> <br />
+            <span>Hálova 9</span> <br />
+            <span>Bratislava</span> <br />
+            <span>851 01</span> <br />
+            <span>Slovakia</span> <br />
+            <br />
+            <a href="tel:+421948484490">+421 948 484 490</a> <br />
+            <br />
+            <a href="mailto:martin@zilak.sk">martin@zilak.sk</a> <br />
+            <a href="https://zilak.sk">https://zilak.sk</a>
+          </div>
+
+          <div>
+            <span className={styles.sidebarHeadline}>languages</span> <br />
+            <span className={styles.language}>english&nbsp;</span>
+            <span className={styles.languageLevel}>fluent</span> <br />
+            <span className={styles.language}>slovak&nbsp;</span>
+            <span className={styles.languageLevel}>native</span>
+          </div>
+
+          <div>
+            <span className={styles.sidebarHeadline}>skills</span> <br />
+          </div>
+
+          <div>
+            <span className={styles.sidebarHeadline}>hobbies</span> <br />
+            <span>cooking</span> <br />
+            <span>cycling</span> <br />
+            <span>reading</span> <br />
+          </div>
+        </div>
+
+        <div className={styles.body}>asfafs</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
